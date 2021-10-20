3e10ce4e698d729cf75ee1184e917746dc0bdd5f
Emir Turkes <eturkes@bu.edu>
2021-10-20 14:31:16 +0100

    Add specific peptide analysis

--- R/utils.R
+++ R/utils.R
@@ -462,3 +462,72 @@ plot_detect_custom <- function(se) {
   p
   # -----------------------------------------------------------------
 }
+
+# The following section originates from content on Github
+# https://github.com/jokergoo/ComplexHeatmap/issues/155#issuecomment-551153988
+# Author: Maarten Slagter https://github.com/slagtermaarten
+# ----------------------------------------------------------------------------
+#' Hclust cannot handle matrices in which for some pairs of rows and columns,
+#' only 1 or fewer shared values are non-NA. This function recurrently
+#' identifies the most aggravating column/row, excludes that column/row and checks
+#' whether more columns/rows need to be excluded
+#'
+#' @param mat Matrix to investigate
+#' @param min_shared_fields Minimum number of positions that are not NA in both
+#' vectors in order not to flag the vector pair as problematic
+#'
+identify_problematic_combs <- function(mat, min_shared_fields = 1) {
+  exclude_rows <- NULL
+  exclude_cols <- NULL
+  stopifnot(is.matrix(mat))
+
+  ## Loop over candidate removals
+  for (k in 1:nrow(mat)) {
+    candidate_rows <- setdiff(1:nrow(mat), exclude_rows)
+    problem_row_combs <- NULL
+    for (i in candidate_rows) {
+      i_idx <- which(candidate_rows == i)
+      for (j in candidate_rows[i_idx:length(candidate_rows)]) {
+        if (sum(!is.na(mat[i, ]) & !is.na(mat[j, ])) <= min_shared_fields) {
+          problem_row_combs <- rbind(problem_row_combs, c(i, j))
+        }
+      }
+    }
+    if (is.null(problem_row_combs)) break
+    exclude_rows <- c(exclude_rows,
+                      as.integer(names(which.max(table(problem_row_combs)))))
+  }
+
+  for (k in 1:ncol(mat)) {
+    candidate_cols <- setdiff(1:ncol(mat), exclude_cols)
+    problem_col_combs <- NULL
+    for (i in candidate_cols) {
+      i_idx <- which(candidate_cols == i)
+      for (j in candidate_cols[i_idx:length(candidate_cols)]) {
+        if (sum(!is.na(mat[, i]) & !is.na(mat[, j])) <= min_shared_fields) {
+          problem_col_combs <- rbind(problem_col_combs, c(i, j))
+        }
+      }
+    }
+    if (is.null(problem_col_combs)) break
+    exclude_cols <- c(exclude_cols,
+                      as.integer(names(which.max(table(problem_col_combs)))))
+  }
+
+  return(list('row' = exclude_rows, 'column' = exclude_cols))
+}
+
+
+remove_problematic_combs <- function() {
+  problematic_combs <- identify_problematic_combs(
+    mat = mat, min_shared_fields = min_shared_fields)
+  if (!is.null(problematic_combs$row)) {
+    mat <- mat[-problematic_combs$row, ]
+  }
+  if (!is.null(problematic_combs$column)) {
+    mat <- mat[, -problematic_combs$column]
+  }
+  return(mat)
+}
+formals(remove_problematic_combs) <- formals(identify_problematic_combs)
+# ----------------------------------------------------------------------------
